fa1032f91431a67cc2fb6cbc8c0c891dd5c36a2d
Pablo Macon <pablomacon@gmail.com>
2026-05-04 16:05:37 -0300

    arreglamos config de la actividad

--- 2026/pi/variables/02/config.js
+++ 2026/pi/variables/02/config.js
@@ -1,5 +1,6 @@
 window.APP_CONFIG = {
   activitySlug: "variables-java-02",
   apiBaseUrl: "https://backend-ejercicios-pm.vercel.app/api",
-  googleClientId: "TU_GOOGLE_CLIENT_ID"
+  googleClientId:
+    "593049746670-phfmkb7ed6dbr48bpo268r8agc41jj6h.apps.googleusercontent.com",
 };
