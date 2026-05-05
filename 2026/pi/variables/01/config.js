70ee95d1eb16b126b2cabe153d3b5cfd421a26a0
Pablo Macon <pablomacon@gmail.com>
2026-05-05 10:20:58 -0300

    Arreglamos id cliente actividad 01

--- 2026/pi/variables/01/config.js
+++ 2026/pi/variables/01/config.js
@@ -2,5 +2,5 @@ window.APP_CONFIG = {
   activitySlug: "variables-java-01",
   apiBaseUrl: "https://backend-ejercicios-pm.vercel.app/api",
   googleClientId:
-    "593049746670-phfmkb7ed6dbr48bpo268r8agc41jj6h.apps.googleusercontent.com"
+    "593049746670-phfmkb7ed6dbr48bpo268r8agc41jj6h.apps.googleusercontent.com",
 };
