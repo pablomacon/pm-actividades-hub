2406515eeda4ff068e783ec1e39e80ec5aef1ed9
Pablo Macon <pablomacon@gmail.com>
2026-07-20 16:29:59 -0300

    Organizar actividades iterativas y agregar actividad 2

---
+++ 2026/pi/iterativas/02/config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  ...window.PM_SHARED_CONFIG,
+  activitySlug: "iterativas-java-02",
+};
---
+++ 2026/pi/iterativas/02/sql_actividad_2.sql
@@ -0,0 +1,29 @@
+-- Actividad 2 - Repetición y decisiones en Java
+-- Grupo: 1MF | Docente: Prof. Pablo Macón
+
+INSERT INTO actividades (slug, titulo, activa, anio, asignatura, tema, orden, url, descripcion)
+VALUES (
+  'iterativas-java-02',
+  'Repetición y decisiones en Java - Actividad 2',
+  TRUE,
+  2026,
+  'pi',
+  'iterativas',
+  2,
+  '/2026/pi/iterativas/02/',
+  'Actividad autocorregible sobre repetición, decisiones, acumuladores, centinelas y banderas en Java.'
+);
+
+INSERT INTO preguntas (actividad_slug, numero_pregunta, respuesta_correcta) VALUES
+('iterativas-java-02', 1, 'a'), ('iterativas-java-02', 2, 'a'),
+('iterativas-java-02', 3, 'a'), ('iterativas-java-02', 4, 'a'),
+('iterativas-java-02', 5, 'a'), ('iterativas-java-02', 6, 'a'),
+('iterativas-java-02', 7, 'a'), ('iterativas-java-02', 8, 'a'),
+('iterativas-java-02', 9, 'a'), ('iterativas-java-02', 10, 'a'),
+('iterativas-java-02', 11, 'b'), ('iterativas-java-02', 12, 'a'),
+('iterativas-java-02', 13, 'a'), ('iterativas-java-02', 14, 'a'),
+('iterativas-java-02', 15, 'a'), ('iterativas-java-02', 16, 'a'),
+('iterativas-java-02', 17, 'a'), ('iterativas-java-02', 18, 'a'),
+('iterativas-java-02', 19, 'a'), ('iterativas-java-02', 20, 'a');
+
+-- Habilitación para 1MF: ejecutar según el criterio de asignación del backend.
